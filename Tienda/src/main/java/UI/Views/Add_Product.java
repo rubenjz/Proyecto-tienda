ef0dfcc0a43a7582f10eb3871073a3a9f1483c92
rubenjz <jimenezruben304@gmail.com>
2023-10-02 14:12:37 -0500

    finalización de la pantalla add_product y se añade una copia de la base de datos para pruebas

--- Tienda/src/main/java/UI/Views/Add_Product.java
+++ Tienda/src/main/java/UI/Views/Add_Product.java
@@ -1,5 +1,8 @@
 package UI.Views;
 
+import Controler.DAOProductoImpl;
+import Interfaces.DAOProducto;
+import Model.Producto;
 import java.awt.Image;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -35,14 +38,23 @@ public class Add_Product extends javax.swing.JPanel {
         jLabel5 = new javax.swing.JLabel();
         jSeparator5 = new javax.swing.JSeparator();
         codigoP = new javax.swing.JTextField();
-        jLabel6 = new javax.swing.JLabel();
         jSeparator6 = new javax.swing.JSeparator();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
         jLabel1.setText("Ingrese el nombre del producto:");
 
         jLabel2.setText("Ingrese el precio del distribuidor del producto:");
+
+        costoCompraP.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                costoCompraPActionPerformed(evt);
+            }
+        });
 
         jLabel3.setText("Ingrese el precio de venta del producto:");
 
@@ -51,6 +63,50 @@ public class Add_Product extends javax.swing.JPanel {
         jLabel5.setText("Ingrese el código del producto:");
 
         jSeparator6.setOrientation(javax.swing.SwingConstants.VERTICAL);
+
+        jPanel2.setBackground(new java.awt.Color(255, 255, 255));
+
+        jButton1.setBackground(new java.awt.Color(255, 102, 102));
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Registrar Producto");
+        jButton1.setBorder(null);
+        jButton1.setBorderPainted(false);
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        jLabel6.setText("Nota: Si va a ingresar una cantidad decimal utilice punto \" . \"");
+
+        jLabel7.setText("Ejemplo: 2.50");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(jLabel7))
+                        .addGap(0, 2, Short.MAX_VALUE)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel6)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 70, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(125, 125, 125))
+        );
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -71,24 +127,22 @@ public class Add_Product extends javax.swing.JPanel {
                     .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(costoCompraP, javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(jSeparator2)
-                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 353, Short.MAX_VALUE)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 364, Short.MAX_VALUE)
                     .addComponent(nombreP)
                     .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
-                .addComponent(jSeparator6, javax.swing.GroupLayout.DEFAULT_SIZE, 18, Short.MAX_VALUE)
+                .addComponent(jSeparator6, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 340, Short.MAX_VALUE)
-                .addContainerGap())
+                .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(51, 51, 51))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(82, 82, 82)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(jSeparator6)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(14, 14, 14)
-                        .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
@@ -134,18 +188,78 @@ public class Add_Product extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        String Text;
+        String nombre = nombreP.getText();
+        double costoCompra = 0;
+        double precioVenta = 0;
+        int cantidad = 0;
+        int codigo = 0;
+        
+        try {
+            Text = costoCompraP.getText();
+            if (!Text.isEmpty()) {
+                costoCompra = Double.parseDouble(costoCompraP.getText());
+            } else {
+                javax.swing.JOptionPane.showMessageDialog(this, "El campo del precio del distribuidor no debe quedar vacio\n", "AVISO", javax.swing.JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            Text = precioVentaP.getText();
+            if (!Text.isEmpty()) {
+                precioVenta = Double.parseDouble(precioVentaP.getText());
+            } else {
+                javax.swing.JOptionPane.showMessageDialog(this, "El campo de precio de venta no debe quedar vacio\n", "AVISO", javax.swing.JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+            Text = cantidadP.getText();
+            if (!Text.isEmpty()) {
+                cantidad = Integer.parseInt(cantidadP.getText());
+            }
+            Text = codigoP.getText();
+            if (!Text.isEmpty()) {
+                codigo = Integer.parseInt(Text);
+            }
+        } catch (NumberFormatException e) {
+            javax.swing.JOptionPane.showMessageDialog(this, "Revise si ingreso una coma o\ncarácter en un campo numérico", "AVISO", javax.swing.JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        Producto producto = new Producto(codigo, nombre, cantidad, precioVenta, costoCompra);
+        
+        try {
+            DAOProducto dao = new DAOProductoImpl();
+            dao.registrar(producto);
+            javax.swing.JOptionPane.showMessageDialog(this, "Se registro el producto exitosamente\n", "AVISO", javax.swing.JOptionPane.INFORMATION_MESSAGE);
+            
+            nombreP.setText("");
+            costoCompraP.setText("");
+            precioVentaP.setText("");
+            cantidadP.setText("");
+            codigoP.setText("");
+        } catch (Exception e) {
+            javax.swing.JOptionPane.showMessageDialog(this, "Ocurrió un problema al registrar el producto", "AVISO", javax.swing.JOptionPane.ERROR_MESSAGE);
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void costoCompraPActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_costoCompraPActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_costoCompraPActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField cantidadP;
     private javax.swing.JTextField codigoP;
     private javax.swing.JTextField costoCompraP;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator3;
